626a4d01677bb2d874bbb298ef8814240439de6f
Sujith <sujithma1992@gmail.com>
2015-09-29 18:36:55 +0530

    Theme Changed To Grey and Android Platform Added

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,6 +1,17 @@
 angular.module('home.controllers', [])
 
 .controller('homeCtrl', function($scope, $state, $ionicPlatform) {
+	$scope.products=[
+    {
+      name: "AiA",
+      quantity: "200"
+    },
+    {
+      name: "Cargills",
+      quantity: "200"
+     
+    }
+  ]
 	
 });
 
